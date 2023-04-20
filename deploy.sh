0b5ca7dcd604098224b069477a9cd80881709b68
Chad Voegele <chad@voegele.me>
2023-04-19 21:35:27 -0500

    Switch to Amazon Bedrock

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 BUCKET_NAME="www.voegele.me"
 
+if [[ ! -d _site ]]
+then
+  echo "No _site found!"
+  exit 1
+fi
+
+if [[ -z $AWS_SECRET_ACCESS_KEY ]]
+then
+  echo "No aws credentials!"
+  exit 1
+fi
+
 aws s3 sync --delete _site s3://${BUCKET_NAME}
 
 for f in $(find _site -mindepth 2 -type f -name "index.html")
